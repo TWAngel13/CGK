4c90d8e766f4567f409e21b4062117fc3c7d12c6
SAANN3 <95036865+SAANN3@users.noreply.github.com>
2024-03-26 01:54:11 +0500

    moved all api URL's to /api/*

--- src/app.js
+++ src/app.js
@@ -6,11 +6,11 @@ const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const placesRoute = require("./routes/places")
 app.use(express.json());
-app.use("/auth/",authRoute)
-app.use("/users/",usersRoute)
-app.use("/objects/",placesRoute)
+app.use("/api/auth/",authRoute)
+app.use("/api/users/",usersRoute)
+app.use("/api/objects/",placesRoute)
 // Error handling
-app.get('*', function(req, res){
+app.get('/api/*', function(req, res){
     res.status(404).send({error:"Not Found"});
 });
 app.listen(port,() => {console.log(`Listening on port:${process.env.PORT}`)});
