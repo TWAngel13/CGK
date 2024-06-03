8f7c0bb8ad745cf89d7cd6dc9b46133fba4547a1
TWAngel13 <annet_fomina2002@mail.ru>
2024-06-03 17:13:33 +0500

    на пути к поиску

--- public/js/objectsCommon.js
+++ public/js/objectsCommon.js
@@ -5,9 +5,14 @@ var position = 0;
 
 export async function getObjectsOfCategory(category, number = 1)
 {
-    //console.log("startPos: " + position + " maxPlaces: " + number + "objectCategory")
-    //console.log((await api.getObjectsList(undefined, position, number, undefined, category)))
     var result = (await api.getObjectsList(undefined, position, number, undefined, category)).objects
+    position += number;
+    return result;
+}
+
+export async function searchObjects(searchString, number = 1)
+{
+    var result = (await api.getObjectsList(searchString, position, number, undefined, undefined)).objects
     position += number;
     return result;
 }
